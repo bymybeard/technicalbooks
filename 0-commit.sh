abd2360bf203d292e9c083bc7acf4f1d7f8df0be
bymybeard <105630851+bymybeard@users.noreply.github.com>
2022-06-09 22:10:53 +1000

    created PS folder, added books

---
+++ 0-commit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+msg=$1
+
+function add_files() {
+    echo "[ INFO ] added all the files"
+    git add --all
+
+}
+
+function commit_message() {
+    echo "[ INFO ] committed with the message --> $msg"
+    git commit -m "$msg"
+
+}
+
+function push_remote() {
+    echo "[ INFO ] pushing to remote"
+    git push
+
+}
+
+
+add_files
+commit_message
+push_remote
